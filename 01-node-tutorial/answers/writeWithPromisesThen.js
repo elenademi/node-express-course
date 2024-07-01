2fb2e25ce264e1d8227457dea4624023652cdbba
elenademi <118300498+elenademi@users.noreply.github.com>
2024-06-30 22:02:15 -0400

    revised answers to week 2

--- 01-node-tutorial/answers/writeWithPromisesThen.js
+++ 01-node-tutorial/answers/writeWithPromisesThen.js
@@ -1,23 +1,22 @@
-const { writeFile, readFile } = require("fs").promises; 
-const firstThenLine = "There are plenty of meat substitutes available.\n"
-const secondThenLine = "Vegan cheese tastes amazing!\n"
-const thirdThenLine = "And tofu is really yummy if you cook it the right way, which, by the way, is quick and easy.\n"
+const { writeFile, readFile } = require("fs").promises;
+const firstThenLine = "There are plenty of meat substitutes available.\n";
+const secondThenLine = "Vegan cheese tastes amazing!\n";
+const thirdThenLine =
+  "And tofu is really yummy if you cook it the right way, which, by the way, is quick and easy.\n";
 
-writeFile("./answers/temp.txt", `${firstThenLine}`)   
- .then(() => {  
-    return writeFile(
-        './answers/temp.txt',
-        `${secondThenLine}`,{ flag: 'a' })    
-     })  
- .then (() => {  
-    return writeFile(
-        './answers/temp.txt',
-        `${thirdThenLine}`,{ flag: 'a' })    
-     }) 
+writeFile("temp.txt", `${firstThenLine}`)
   .then(() => {
-        return readFile("./answers/temp.txt", "utf8"); 
-      }) 
-   .then((result) => { console.log(result); })     
- .catch((error) => {  
-     console.log("An error occurred: ", error)  
- })  
+    return writeFile("temp.txt", `${secondThenLine}`, { flag: "a" });
+  })
+  .then(() => {
+    return writeFile("temp.txt", `${thirdThenLine}`, { flag: "a" });
+  })
+  .then(() => {
+    return readFile("temp.txt", "utf8");
+  })
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log("An error occurred: ", error);
+  });
