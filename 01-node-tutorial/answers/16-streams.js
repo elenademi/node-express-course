5bf59a8043c68f8d5bc310ab94cce67fd30c7d9d
elenademi <118300498+elenademi@users.noreply.github.com>
2024-07-11 00:17:43 -0400

    revised file 16-streams.js

--- 01-node-tutorial/answers/16-streams.js
+++ 01-node-tutorial/answers/16-streams.js
@@ -1,9 +1,12 @@
 const { createReadStream } = require('fs')
-const stream = createReadStream('./content/big.txt', { highWaterMark: 200, encoding: 'utf8' },)
+let counter = 0;
+const stream = createReadStream('../content/big.txt', { highWaterMark: 200, encoding: 'utf8' },)
 stream.on('data', (result) => {
-  console.log("Received data",result)
+  counter++;
+  console.log("Received data", result)
 })
 stream.on('end', () => {
     console.log('End of data.');
+    console.log("Number of chunks received:", counter);
   });
 stream.on('error', (err) => console.log(err))
