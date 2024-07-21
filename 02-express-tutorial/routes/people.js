d251d074be418b91ab2fab75621724ec163228de
elenademi <118300498+elenademi@users.noreply.github.com>
2024-07-21 01:23:04 -0400

    updated the files so that updating and deleting people modifies the original array

--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -1,12 +1,11 @@
 const express = require("express");
-let {people } = require('../data')
+let {people } = require("../data")
 const { addPerson, getPeople, findPerson, updatePerson, deletePerson } = require("../controllers/people.js");
 const router = express.Router();
 router.get("/", getPeople);
-
 router.post("/", addPerson);
 router.get("/:id", findPerson);
-router.put('/:id', updatePerson)
-router.delete('/:id', deletePerson)
+router.put("/:id", updatePerson)
+router.delete("/:id", deletePerson)
 
 module.exports = router;
